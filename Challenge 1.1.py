eff68945cce50a38edab122c338ad756344ada4a
Vijipalraj2004 <147394221+Vijipalraj2004@users.noreply.github.com>
2023-10-09 17:00:28 +0530

    Delete Challenge 1.1.py

--- Challenge 1.1.py
+++
@@ -1,8 +0,0 @@
-def fact_rect(n):
-  if n==0 or n==1:
-    return 1
-  else:
-    return n*fact_rect(n-1)
-number=int(input ('Enter the Factorial number in int ')) 
-result=int(fact_rect(number)) 
-print("The Factorial of {} is {}".format(number,result))
